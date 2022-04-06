05038e36aac337d9f7e1d97f14311f78bf37a31a
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2022-04-06 16:30:41 +0900

    Add files via upload

---
+++ js/weddingflyer.js
@@ -0,0 +1,7 @@
+// JavaScript Do
+function on_pay() { 
+ myform = document.mobileweb; 
+ myform.action = "https://mobile.inicis.com/smart/payment/";
+ myform.target = "_self";
+ myform.submit(); 
+}  
