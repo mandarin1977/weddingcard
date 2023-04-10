1629f493930b0da415e7a989cf306fbd0aab87e8
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2023-04-10 10:28:08 +0900

    Delete js directory

--- js/weddingflyer.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Do
-function copy_to_clipboard() {
-	var copyText = document.getElementById('myInput');
-	copyText.select();
-	copyText.setSelectionRange(0, 99999);
-	document.execCommand("Copy");
-	alert('복사되었습니다');
-}
